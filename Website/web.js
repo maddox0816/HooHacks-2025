d1607837e2422e839235dd7de2805ae23e690470
1-anshika-s <anshika.au@gmail.com>
2025-03-29 21:53:05 -0400

    website folder

---
+++ Website/web.js
@@ -0,0 +1,95 @@
+// web.js
+var userelectricityUsage = 0;
+var usercarbonEmissions = 0;
+document.addEventListener('DOMContentLoaded', function () {
+    // Add your popup logic here
+
+
+
+
+    function updateComparisons(){
+        document.getElementById("electricity-usage").textContent = "10";
+    }
+
+
+    // Begin by getting emmision and electricity usage data from the storage
+    let emmisions = 0;
+    let electricity = 0;
+    //get visits from local storage
+    chrome.storage.local.get(['visits'], (result) => {
+        console.log('Visits:', result.visits);
+        let visits = result.visits || [];
+
+        // Loop through the visits and add up the emissions and electricity usage
+        for (let i = 0; i < visits.length; i++) {
+            emmisions += visits[i].emissions;
+            electricity += visits[i].electricity;
+        }
+
+        // Display the emissions and electricity usage in the popup
+        document.getElementById('electricity-usage').textContent = electricity.toFixed(2);
+        document.getElementById('carbon-emissions').textContent = emmisions.toFixed(2);
+    })
+
+    function calculateNumOfiPhoneCharges()
+    {
+        const electricityUsageElement = document.getElementById("electricity-usage");
+        const iPhoneChargesElement = document.getElementById("iPhone-Charges");
+        const numiPhoneChargesElement = electricityUsageElement * 0.0178;
+
+        //Display number of iPhone Charges
+        document.getElementById('iPhone-Charges').textContent = numiPhoneChargesElement.toFixed(2);
+    }
+
+
+    function getActiveTabURL(callback) {
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            if (tabs.length > 0) {
+                callback(tabs[0].url);
+            }
+        })
+    }
+
+    async function checkGreenStatus(domain)  {
+        // gather info from the api
+        // register if domain is in database or not
+        try {
+            const response = await fetch(`https://api.thegreenwebfoundation.org/api/v3/greencheck/${domain}`);
+            const data = await response.json();
+            console.log("API Response", data);
+            return data;
+        }
+
+            //Catch error if website not in API database
+        catch (error) {
+            console.error("Website not in database:", error);
+            return null;
+        }
+    }
+
+    //get and test the active tab URL
+    getActiveTabURL((URL) => {
+        console.log("Active Tab URL:", URL);
+        const domain = new URL(URL).hostname; //Extract domain from the URL
+
+        checkGreenStatus(domain).then((data) => {
+            const resultElement = document.getElementById("result");
+
+            if (data && data.green !== undefined) {
+                const isGreen = data?.green ?? null; // If green is missing default to null
+                // Conditional to determine if a website is green or not
+                resultElement.textContent = isGreen === null
+                    ? "⚠️ Green status is unidentifiable"
+                    : isGreen
+                        ? `✅ ${domain} is hosted on green energy`
+                        : `❌ ${domain} is NOT hosted on green energy`;
+
+            }
+
+            else {
+                resultElement.textContent = "⚠️ Green status is unidentifiable";
+            }
+        });
+
+    })
+});
