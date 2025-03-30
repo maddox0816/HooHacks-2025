8872b9990ec51338007df7a6f02526344391b86d
DrewH711 <drex47127@gmail.com>
2025-03-29 22:46:23 -0400

    added carbon rating to popup

--- popup.js
+++ popup.js
@@ -3,6 +3,7 @@
 document.addEventListener("DOMContentLoaded", async function () {
     const electricityUsageElement = document.getElementById("electricity-usage");
     const carbonEmissionsElement = document.getElementById("carbon-emissions");
+    const carbonRating = document.getElementById("carbon-rating-value")
 
     // Get the current active tab
     chrome.tabs.query({ active: true, currentWindow: true }, async function (tabs) {
@@ -26,9 +27,11 @@ document.addEventListener("DOMContentLoaded", async function () {
             // Extract energy and CO2 data
             const energyKWh = data.statistics.energy; // Energy in kWh
             const co2Grams = data.statistics.co2.grid.grams; // CO2 in grams
+            const carbonRatingValue = data.statistics.carbonRating;
 
             electricityUsageElement.textContent = (energyKWh * 1000).toFixed(10); // Convert to Wh
             carbonEmissionsElement.textContent = (co2Grams / 1000).toFixed(10); // Convert to kg
+            carbonRating.textContent = carbonRatingValue;
         } catch (error) {
             console.error("Error fetching data:", error);
             electricityUsageElement.textContent = "Data unavailable";
