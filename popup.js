707a8c43fbf87255223c62c3c5bf67a66a13690f
1-anshika-s <anshika.au@gmail.com>
2025-03-29 20:26:05 -0400

    button goes to chatgpt

--- popup.js
+++ popup.js
@@ -4,6 +4,11 @@ document.addEventListener('DOMContentLoaded', function () {
     // Add your popup logic here
     console.log('Popup loaded successfully!');
 
+   //make button a link
+    document.getElementById('emission-breakdown-button').addEventListener('click', function() {
+        window.open('http://chatgpt.com', '_blank');
+    });
+
     function getActiveTabURL(callback) {
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
             if (tabs.length > 0) {
