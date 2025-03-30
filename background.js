5139955efe87a7551ccf6fbb0b39af472b2239ff
Maddox Nichols <fpc6dg@virginia.edu>
2025-03-29 21:08:59 -0400

    Implement backend list of lists

--- background.js
+++ background.js
@@ -1,21 +1,32 @@
 // Listen for messages from the content script
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.type === 'INCREMENT_COUNTER') {
-        // Get the current counter from local storage
-        chrome.storage.local.get(['searchCounter'], (result) => {
-            let counter = result.searchCounter || 0;
-
-            // Increment the counter
-            counter++;
-
-            // Save the updated counter back to local storage
-            chrome.storage.local.set({ searchCounter: counter }, () => {
-                console.log(`Search counter updated: ${counter}`);
-            });
-        });
-    }
     if( message.type === 'NEW_VISIT') {
         // Handle new visit message
-        console.log('New visit:', message.hostname, message.seconds, message.date);
+        console.log('New visit:', message.hostname, message.date);
+
+        let electircityUsed = 13
+        let emissions = 3.5
+        
+        //create a list of the hostname, seconds, emissions, electricity, and date
+        let visitData = {
+            date: message.date,
+            electricity: electircityUsed,
+            emissions: emissions,
+            hostname: message.hostname,
+        };
+
+        //append to the list of lists in chrome.storage.local
+        chrome.storage.local.get(['visits'], (result) => {
+            let visits = result.visits || [];
+            visits.push(visitData);
+            console.log('Visits:', visits);
+
+            // Save the updated visits back to local storage
+            chrome.storage.local.set({ visits: visits }, () => {
+                console.log('Visits updated:', visits);
+                sendResponse({ status: 'success', data: visits });
+            });
+        });
+        
     }
 });
--- googleTest.js
+++ googleTest.js
@@ -1,4 +1,4 @@
 console.log("Background script loaded and listening for messages.");
-chrome.runtime.sendMessage({ type: 'NEW_VISIT', hostname: window.location.hostname, seconds: 3, date: new Date().toISOString() }, (response) => {
-    console.log('Response from background script:', response);
+chrome.runtime.sendMessage({ type: 'NEW_VISIT', hostname: window.location.hostname, date: new Date().toISOString() }, (response) => {
+    console.log('Response from background script:', response.data);
 });
