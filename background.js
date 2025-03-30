b8284a7f37a5d30d7c9488dfa9867cee2d5a4f68
Maddox Nichols <fpc6dg@virginia.edu>
2025-03-29 21:41:48 -0400

    Implement beta databse implementation

--- background.js
+++ background.js
@@ -4,8 +4,10 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         // Handle new visit message
         console.log('New visit:', message.hostname, message.date);
 
-        let electircityUsed = 13
-        let emissions = 3.5
+        let electircityUsed = 0.6 * message.dataSize; // kWh
+        // Example calculation for emissions based on electricity used
+        let emissions = 0.3175 * electircityUsed; // kg CO2 (assuming 0.3175 kg CO2 per kWh)
+        console.log('Electricity used:', electircityUsed, 'kWh');
         
         //create a list of the hostname, seconds, emissions, electricity, and date
         let visitData = {
--- googleTest.js
+++ googleTest.js
@@ -1,4 +1,29 @@
 console.log("Background script loaded and listening for messages.");
-chrome.runtime.sendMessage({ type: 'NEW_VISIT', hostname: window.location.hostname, date: new Date().toISOString() }, (response) => {
-    console.log('Response from background script:', response.data);
-});
+//wait for dom to load
+function onDomLoaded(){
+    console.log("DOM fully loaded");
+    //get the size of data used to load the tab
+    var dataSize = performance.getEntriesByType("navigation")[0].transferSize;
+    console.log("Data size:", dataSize);
+
+    chrome.runtime.sendMessage({ type: 'NEW_VISIT', hostname: window.location.hostname, date: new Date().toISOString(), dataSize: dataSize }, function (response) {
+        console.log('Response from background script:', response.data);
+    });
+    
+
+    console.log("Message sent to background script with dataSize:", dataSize);
+
+}
+
+
+if(document.readyState === 'loading') {
+    // Loading hasn't finished yet
+    document.addEventListener('DOMContentLoaded', function() {
+        console.log("DOM fully loaded and parsed");
+        onDomLoaded();
+    });
+} else {
+
+    onDomLoaded();
+}
+
